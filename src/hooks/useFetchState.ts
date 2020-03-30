d1f4726f03eb172e995b369d4a54746209b9afa7
Max Günther <max.guenther.private@gmail.com>
2020-03-30 09:36:39 +0200

    useHooks without context II

--- src/hooks/useFetchState.ts
+++ src/hooks/useFetchState.ts
@@ -3,7 +3,12 @@ import { FetchContext } from "../context/FetchProvider";
 import { IFetchStateResult } from "../types";
 
 export default (): IFetchStateResult => {
-  const { loading, error } = React.useContext(FetchContext);
+  const fetchContext = React.useContext(FetchContext);
+  if (!fetchContext)
+    throw new Error(
+      "Please do not use this hook if you do not specify a FetchProvider"
+    );
+  const { loading, error } = fetchContext;
   return {
     loading,
     error
